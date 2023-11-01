aade57a319e8226ee05038a5261e4bf95d2d824f
Ayron Felipe <ayron41@gmail.com>
2023-11-01 16:14:33 -0300

    feat: creating admin

--- challenge/apps/core/admin.py
+++ challenge/apps/core/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from challenge.apps.core.models import Message
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    pass
--- challenge/apps/users/admin.py
+++ challenge/apps/users/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+
+from challenge.apps.users.models import UserData
+
+
+@admin.register(UserData)
+class UserDataAdmin(admin.ModelAdmin):
+    pass
